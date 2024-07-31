23777f6489ca3d3e86fedc2723cead5cf3c00381
Divakar P S <99823119+DivakarPS@users.noreply.github.com>
2024-07-31 22:21:34 +0530

    Delete src directory

--- src/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const ProblemRouter  = require('./problems.routes.js');
-
-const v1Router = express.Router();
-
-v1Router.use('/problems', ProblemRouter);
-
-module.exports = v1Router;
